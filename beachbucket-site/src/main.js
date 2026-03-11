3c88d2b43c4a114ff0faa32484c289cec45710bc
Myrie <myriework@gmail.com>
2026-03-11 13:52:36 -0400

    Add Seafood Omelet menu image

--- beachbucket-site/src/main.js
+++ beachbucket-site/src/main.js
@@ -201,7 +201,8 @@ const featuredItemImages = {
   'Bucket Burger': withBase('/assets/menu/bucket-burger.jpg'),
   'Chicken Tender': withBase('/assets/menu/chicken-tender.jpg'),
   'Fried Shrimp': withBase('/assets/menu/fried-shrimp-plate.jpg'),
-  '#4 The Big Beach': withBase('/assets/menu/big-beach.jpg')
+  '#4 The Big Beach': withBase('/assets/menu/big-beach.jpg'),
+  'Seafood Omelet': withBase('/assets/menu/seafood-omelet.jpg')
 }
 
 const menuNamesEn = menuSections.flatMap(s => s.items.map(i => i[0]))
